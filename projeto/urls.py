12e38cedc29896ab5b20eb8e61d0321d8178c5ea
Benjamin Zerpa <bzerpam10@gmail.com>
2022-09-09 19:47:54 -0300

    first url path

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def my_view(request):
+    return HttpResponse('Uma linda string')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),
 ]
