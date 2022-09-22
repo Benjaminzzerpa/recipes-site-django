1f14df0d7c8f061ee974b279c6de71bf1422030d
Benjamin Zerpa <bzerpam10@gmail.com>
2022-09-21 23:28:25 -0300

    first css style

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'benjamin zerpa',       
     }) 
 
