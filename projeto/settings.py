a546ba161d7c21d4a2df37db920fccc3d73eb718
Benjamin Zerpa <bzerpam10@gmail.com>
2022-10-06 20:23:58 -0300

    block for each title

--- projeto/settings.py
+++ projeto/settings.py
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+                    BASE_DIR / 'base_templates',
+                    BASE_DIR / 'recipes' / 'templates_temp'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
