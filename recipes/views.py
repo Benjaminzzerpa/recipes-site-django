e7d1f02fc6c2a58108dacdbd1c97a356543ec471
Benjamin Zerpa <bzerpam10@gmail.com>
2022-10-06 23:04:03 -0300

    data in list view

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'benjamin zerpa',       
-    }) 
+        'recipes': [make_recipe() for _ in range(10)],      
+    })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe_view.html', context={
-        'name': 'benjamin zerpa',       
+        'recipe': make_recipe(),     
     }) 
 
 
