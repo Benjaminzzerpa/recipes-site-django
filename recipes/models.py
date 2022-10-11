324efa83c797714bb3d3b66a18cb60ba933f61ba
Benjamin Zerpa <bzerpam10@gmail.com>
2022-10-10 21:58:09 -0300

    first migrate

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 
 class Recipe(models.Model):
@@ -15,4 +20,7 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
