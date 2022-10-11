724410edbdee308a74952c3521fb82c383209673
Benjamin Zerpa <bzerpam10@gmail.com>
2022-10-11 20:49:59 -0300

    admin models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
+
