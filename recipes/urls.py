fab48575245a2905b9e0362f9403d3cc4924eb00
Benjamin Zerpa <bzerpam10@gmail.com>
2022-10-20 20:28:58 -0300

    self urls settings

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,6 +6,7 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('recipes/Category/<int:category_id>/', views.category, name="category"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
 
 ]
