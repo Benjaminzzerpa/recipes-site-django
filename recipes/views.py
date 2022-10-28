2ef6c1bf6e48295c33f12f24da54b30886933d88
Benjamin Zerpa <bzerpam10@gmail.com>
2022-10-27 23:53:06 -0300

    url Category

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
@@ -14,8 +15,13 @@ def home(request):
 def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id, is_published=True).order_by('-id')
+    
+    if not recipes:
+        raise Http404('Not found')
+    
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,     
+        'recipes': recipes,   
+        'title': f'{recipes.first().category.name} - Category | '  
     })
 
 
