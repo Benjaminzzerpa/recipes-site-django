515bf112adb525c8ec0cc34161667210bfde10dc
Benjamin Zerpa <bzerpam10@gmail.com>
2022-11-06 23:12:26 -0300

    recipe details

--- recipes/views.py
+++ recipes/views.py
@@ -1,35 +1,41 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 from utils.recipes.factory import make_recipe
 
 from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,     
     })
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id')
-    
-    if not recipes:
-        raise Http404('Not found')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
     
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,   
-        'title': f'{recipes.first().category.name} - Category | '  
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
+   
     })
 
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.get(id=id)
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
     return render(request, 'recipes/pages/recipe_view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,     
     }) 
 
